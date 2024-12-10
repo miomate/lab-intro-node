515f6a8e6acdea072ddfc8228355d893b457e55f
miomate <mrspec@outlook.com>
2024-12-10 01:00:59 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = 0; 
+  }
 
-  add(item) {}
+  //iteration 2
+  add(item) {
+    this.items.push(item); 
+    this.items.sort((a, b) => a - b); // Sort the array in ascending order
+    this.length = this.items.length; // Update the length property
+  }
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds"); // Throw error if position is invalid
+    }
+    return this.items[pos]; // Return the item at the specified index
+  }
 
-  get(pos) {}
 
-  max() {}
 
-  min() {}
 
-  sum() {}
 
-  avg() {}
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); 
+    }
+    return this.items[this.length - 1]; 
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); 
+    }
+        return this.items[0];
+
+  }
+
+
+  sum() {
+    return this.items.reduce((acc, curr) => acc + curr, 0); // Sum all items
+  }
+
+
+
 }
 
 module.exports = SortedList;
